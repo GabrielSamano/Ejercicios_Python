8407a9b549261e6aa0c3562f4bc0bb6e08a0e1b1
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-04-12 12:16:12 -0500

    Add files via upload

---
+++ adivinar.py
@@ -0,0 +1,23 @@
+import random
+
+def advina_el_numero(x):
+    print("===========")
+    print("Welcome")
+    print("Tienes que adivinar el numero genrado por la computadora")
+
+    numero_aleatorio = random.randint(1, x) # Numeros entre 1 y x
+
+    prediccion = 0
+
+    while prediccion != numero_aleatorio:
+        prediccion = int(input(f"adivina entre 1 y {x}: "))
+
+        if prediccion < numero_aleatorio:
+            print("Intentalo otra vez es muy pequeño")
+        elif prediccion > numero_aleatorio:
+            print("Intentalo otra vez es muy grande")
+
+    print(f"!Felicitaciones adivinaste el numero {numero_aleatorio}")
+
+advina_el_numero(10)
+
