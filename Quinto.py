05bb7476fa99906c40de5440ad7e3f7899e920ba
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-05-20 09:51:16 -0500

    Add files via upload

---
+++ Quinto.py
@@ -0,0 +1,36 @@
+# Modificar los String.
+
+#Mayusculas
+a= "Hola Mundo"
+print(a.upper());
+
+#Minusculas
+a= "HOLA Mundo"
+print(a.lower());
+
+#Eliminar espacios.
+a= "Hola,Mundo! "
+print(a.strip());
+
+#Reemplazar texto.
+a= "Hola Mundo"
+print(a.replace("H","J"));
+
+#Dividir string.
+a= "Hola, Mundo"
+print(a.split(","));
+
+#Concatenacion.
+a= " Hola Mundo "
+b= " Python es asombroso "
+c= a + b
+print(c);
+
+#Combinar formatos de texto.
+hola= "Saludos a todo desde el año {}"
+year= 2022
+print(hola.format(year));
+
+#Caracteres de escape.
+saludo= "We are the so-called \"Vikings\" from the north."
+print(saludo);
