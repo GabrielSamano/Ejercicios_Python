9837c9e3505d5c4a6772de8c8044bc4e0fc44526
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-06 09:32:25 -0500

    Conjuntos y Diccionarios

---
+++ octavo.py
@@ -0,0 +1,25 @@
+# Conjuntos en Python
+first = {"orange", 15, "Wall"};
+print(first);
+print(len(first));
+print(type(first));
+first.add("Manhwa");
+
+# Diccionarios en Python
+
+Fullly = {
+    "Car": "Ferrari",
+    "Color": "Red",
+    "Year": 2015
+};
+print(Fullly);
+print(Fullly["Car"]);
+print(len(Fullly));
+print(type(Fullly));
+x = Fullly.keys()
+y = Fullly.values()
+print(x);
+print(y);
+
+
+
