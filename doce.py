b9e32f4288e21848ae5ae114e3e5d574870b1f43
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-10 13:59:19 -0500

    Iteradores

---
+++ doce.py
@@ -0,0 +1,30 @@
+# Iteradores
+mytup = {"Apple","Sams","Andorid"};
+myit= iter(mytup)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# Creando un Iterador
+class MYiten:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+myclass = MYiten()
+Myiter = iter(myclass)
+
+for x in Myiter:
+    print(x)
+
+
+
