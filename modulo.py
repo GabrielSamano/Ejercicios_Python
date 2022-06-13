57afb66ca0d7ae1693b9a29e874d93eb06f6c3bd
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-06-13 09:45:18 -0500

    Modulos y fechas

---
+++ modulo.py
@@ -0,0 +1,28 @@
+# Modulos
+import mymodule as mx
+
+mx.firtscom("Angel")
+
+c = mx.person["Country"]
+print(c)
+
+
+# Fecha y Hora
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+# Math
+
+import math
+
+x = math.sqrt(81)
+print(x)
+
+y = math.ceil(3.4)
+z = math.floor(3.4)
+
+print(y)  # return 4
+print(z) # return 3
+
+
