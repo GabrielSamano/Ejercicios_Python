c020cefaa0f5476915273c6c3dca2ad382516195
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-12-01 09:42:12 -0600

    Agregando resta a la calculadora

--- Calculadora-basic.py
+++ Calculadora-basic.py
@@ -1,3 +1,8 @@
 def add(a,b):
     answer = a + b
     print(str(a) + "+" + str(b) + "=" + str(answer))
+    
+def sub(a,b):
+    answer = a - b
+    print(str(a) + "-" + str(b) + "=" + str(answer))
+
