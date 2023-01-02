e7764afff49d9790ebab03ecc8e947997565ec99
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-02 10:42:20 -0600

    Serie de Fibonacci

--- Serie.py
+++ Serie.py
@@ -2,11 +2,11 @@ sum = 0
 num1 = 0
 num2 = 1
 
-while num2 < 4000000:
+while num2 < 4000000: # Ingresa el rango que tu consideres 
     temp = num1
     num1 = num2
     num2 = num2 + temp
-    if (num1 % 2 ==0):
+    if (num1 % 2 ==0): # Utiliza los numeros pares 
         sum += num1
 
 print(sum)
