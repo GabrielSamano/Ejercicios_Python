1e5e3832be6fe06b44430bf8fa8291a6f35f91cb
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-01-17 13:38:33 -0600

    Agregando comentarios

--- BusquedaBinaria.py
+++ BusquedaBinaria.py
@@ -36,7 +36,7 @@ if __name__ == '_main_':
 
     lista_ordenada = sorted(list(conjunto_inicial))
 
-
+# Determinando tiempo de busqueda al azar y binaria con datos random 
 inicio = time.time()
 for objetivo in lista_ordenada:
     Busqueda_azar(lista_ordenada, objetivo)
