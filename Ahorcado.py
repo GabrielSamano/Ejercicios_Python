13836b7143d844b84acc524bb3e40a53b9c4616e
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-03-24 10:27:51 -0600

    Juego del ahorcado

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,1 +1,5 @@
 # En este codigo se muestra un ejemplo de codigo del juego de niños llamado el ahorcado el cual el objetivo es encontrar la palabra escondida
+palabra = "ahorcado"
+errores = 0
+progreso = []
+
