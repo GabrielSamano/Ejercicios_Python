f1553a04365d5aeeb5500cc12fd080434b607c1a
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2023-03-24 11:09:16 -0600

    dibujando el ahorcado

--- Ahorcado.py
+++ Ahorcado.py
@@ -3,9 +3,68 @@ palabra = "ahorcado"
 errores = 0
 progreso = []
 
-# Prueba con un ciclo for 
+ 
 
 for i in range(len(palabra)):
     progreso.append("_ ")
+letras_usadas= []
+# Dibujando el ahorcado
+while errores < 7:
+    if errores == 0:
+        print("_____")
+        print("|   |")
+        print("|    ")
+        print("|    ")
+        print("|    ")
+        print("|    ")
+        print("|____")
+    elif errores == 1:
+        print("_____")
+        print("|   |")
+        print("|   °")
+        print("|    ")
+        print("|    ")
+        print("|    ")
+        print("|____")
+    elif errores == 2:
+        print("_____")
+        print("|   |")
+        print("|   °")
+        print("|   |")
+        print("|    ")
+        print("|    ")
+        print("|____")
+    elif errores == 3:
+        print("_____")
+        print("|   |")
+        print("|   °")
+        print("|   |")
+        print("|  / ")
+        print("|    ")
+        print("|____")
+    elif errores == 4:
+        print("_____")
+        print("|   |")
+        print("|   °")
+        print("|   |")
+        print("|  / \\")
+        print("|    ")
+        print("|____")
+    elif errores == 5:
+        print("_____")
+        print("|   |")
+        print("|   °")
+        print("|  \|")
+        print("|  /\\")
+        print("|    ")
+        print("|____")
+    elif errores == 6:
+        print("_____")
+        print("|   |")
+        print("|   °")
+        print("|  \|/")
+        print("|  /\\")
+        print("|    ")
+        print("|____")
+    break
 
-print(progreso)
